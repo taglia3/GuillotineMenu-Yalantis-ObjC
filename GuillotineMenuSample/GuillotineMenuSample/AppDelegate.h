23cd75fd9c2c4c3a2a88d8f5bc355f1fd7ac6730
taglia <the.taglia3@gmail.com>
2015-12-02 09:17:57 +0100

    Init sample creation

---
+++ GuillotineMenuSample/GuillotineMenuSample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GuillotineMenuSample
+//
+//  Created by Tigielle on 02/12/15.
+//  Copyright © 2015 Matteo Tagliafico. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GuillotineMenuSample/GuillotineMenuSample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GuillotineMenuSample
+//
+//  Created by Tigielle on 02/12/15.
+//  Copyright © 2015 Matteo Tagliafico. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
